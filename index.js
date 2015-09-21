66cb288df1ad3c7e96170468c70d504220601c64
Chema Alvarez <chema.rez@gmail.com>
2015-09-21 22:55:55 +0200

    Rama "Johny" unida a "develop"
    1. Rama "Johny" unida a "develop”.
    2. Archivo package.son creado para llevar un control de los paquetes
    instalados y poder reinstalar o actualizar fácilmente a partir de este
    archivo con los comandos “npm install” o “npm update”. De modo que no
    es necesario guardar en el repositorio la carpeta /node_modules, pero
    sí el package.json
    3. Se ha instalado “nodemon” para controlar la gestión de cambios en
    los archivos directamente sin tener que respetar el servidor

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 	var app = express();
 	
 	app.use('/', function(req, res) {
-		res.send('Hello Motherbase');
+		res.send('Hello Motherbase<br><br><em>Welcome back Boss</em>');
 	});
 	
 	app.listen(8000);
-	console.log('Server running at http://localhost:3000/');
+	console.log('Server running at http://localhost:8000/');
 	
 	module.exports = app;
